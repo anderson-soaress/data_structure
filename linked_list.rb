00523e1ac902de06ee29348fb540dcd485d728e1
Anderson Soares <andersonsoares.jn@gmail.com>
2024-04-16 10:23:05 -0300

    fix: (linked_list.rb) Clear the code

--- linked_list.rb
+++ linked_list.rb
@@ -8,105 +8,109 @@ class LinkedList
   end
 
   def add_head(value)
-    self.head = Node.new(value, nil)  
     self.size += 1
+    self.head = Node.new(value, nil)  
   end
 
-  def append(value)
-    return add_head(value) if head == nil
-
-    node = head
-    until node.next_node == nil do 
-      node = node.next_node
+  def append(value, node = self.head)
+    if head == nil
+      return add_head(value) 
+    elsif node.next_node == nil
+      self.size += 1
+      return node.next_node = Node.new(value, nil)
+    else
+      append(value, node.next_node)
     end
-    node.next_node = Node.new(value, nil)
-    self.size += 1
   end
 
   def prepend(value)
     return add_head(value) if head == nil
-
-    self.head = Node.new(value, head)
     self.size += 1
+    self.head = Node.new(value, head)
   end
 
-  def tail
-    node = head
-    until node.next_node == nil do 
-      node = node.next_node
-    end
-    node
+  def tail(node = self.head)
+    return node if node.next_node == nil
+    tail(node.next_node)
   end
 
-  def at(index)
-    node = head
-    for i in (0..index-1) do 
-      node = node.next_node
-      return nil if node == nil
+  def at(index, node=self.head)
+    if node == nil 
+      return nil
+    elsif index == 0
+      return node
+    else
+      at(index-1, node.next_node)
     end
-    node
   end
 
-  def pop
-    node = head
-    until node.next_node.next_node == nil do 
-      node = node.next_node
+  def pop(node = self.head)
+    if self.size == 1
+      self.size -= 1
+      return self.head = nil 
+    elsif node.next_node.next_node == nil
+      self.size -= 1
+      return node.next_node = nil
+    else
+      pop(node.next_node)
     end
-    node.next_node = nil
   end
 
-  def contains?(value)
-    node = head
-    until node.next_node == nil do 
-      node = node.next_node
-      return true if node.value == value || head.value == value
+  def contains?(value, node=self.head)
+    if node.value == value
+      return true
+    elsif node.next_node == nil
+      return false
+    else
+      contains?(value, node.next_node)
     end
-
-    false
   end
 
-  def find(value)
-    node = head
-    index = 0
-    until node.value == value do 
-      node = node.next_node
-      index += 1
-      return nil if node == nil
+  def find(value, node=self.head, index=0)
+    if node.value == value
+      return index
+    elsif node.next_node == nil
+      return nil
+    else
+      find(value, node.next_node, index+1)
     end
-    index
   end
 
-  def to_s
-    s = "( #{head.value} ) -> "
-    node = head
-    until node.next_node == nil do
-      node = node.next_node
-      s += "( #{node.value} ) -> " 
+  def to_s(node=self.head, s="")
+    if size == 0
+      return "nil"
+    elsif node.next_node == nil
+      return s += "( #{node.value} ) -> nil"
+    else
+      s += "( #{node.value} ) -> "
+      to_s(node.next_node, s)
     end
-    s += "nil"
   end
 
   def insert_at(value, index)
     if index == 0
       return self.prepend(value)
+    elsif index == self.size-1
+      return self.append(value)
     elsif index > self.size-1
       return nil
     else
-      current_node = self.at(index)
-      last_node = self.at(index-1)
-      last_node.next_node = Node.new(value,current_node)
+      self.size += 1
+      self.at(index-1).next_node = Node.new(value,self.at(index))
     end
   end
 
   def remove_at(index)
     if index == 0
+      self.size -= 1
       return self.head = head.next_node
+    elsif index == self.size-1
+      return self.pop
     elsif index > self.size-1
       return nil
     else
-      current_node = self.at(index)
-      last_node = self.at(index-1)
-      last_node.next_node = current_node.next_node
+      self.size -=1
+      self.at(index-1).next_node = self.at(index).next_node
     end
   end
 end
